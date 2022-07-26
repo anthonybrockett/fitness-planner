af8fecb53ddb0ed1226119678838a3438899618b
anthonybrockett <ajbrockett1991@gmail.com>
2022-07-26 16:38:32 -0700

    Update enote routes

--- routes/enotes.js
+++ routes/enotes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const enotesCtrl = require('../controllers/enotes');
+const isLoggedIn = require('../config/auth');
 
 // Post
-router.post('/exercises/:id/enotes', enotesCtrl.create);
+router.post('/exercises/:id/enotes', isLoggedIn, enotesCtrl.create);
 
 // Delete
-router.delete('/enotes/:id', enotesCtrl.delete);
+router.delete('/enotes/:id', isLoggedIn, enotesCtrl.delete);
 
 module.exports = router;
